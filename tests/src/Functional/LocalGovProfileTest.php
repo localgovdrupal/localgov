58a70bd5cb43e1a0b826b210eb2267ea22ceb853
Lee Mills <8024370+millnut@users.noreply.github.com>
2025-02-08 12:30:15 +0000

    fix: calls to module_handler service

--- tests/src/Functional/LocalGovProfileTest.php
+++ tests/src/Functional/LocalGovProfileTest.php
@@ -27,7 +27,7 @@ class LocalGovProfileTest extends BrowserTestBase {
   public function testLocalGovDrupalProfile() {
 
     // Test localgov_core module is enabled and is not uninstallable.
-    $this->assertTrue(\Drupal::service('module_handler')->moduleExists('localgov_core'));
+    $this->assertTrue(\Drupal::moduleHandler()->moduleExists('localgov_core'));
     try {
       \Drupal::service('module_installer')->uninstall(['localgov_core']);
       $this->fail('Uninstalled localgov_core module.');
@@ -38,7 +38,7 @@ class LocalGovProfileTest extends BrowserTestBase {
     }
 
     // Test localgov_core:localgov_roles submodule is enabled.
-    $this->assertTrue(\Drupal::service('module_handler')->moduleExists('localgov_roles'));
+    $this->assertTrue(\Drupal::moduleHandler()->moduleExists('localgov_roles'));
 
     // Test front page loads after site install.
     $this->drupalGet('<front>');
